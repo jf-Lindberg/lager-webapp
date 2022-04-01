e68021c2dcff6da16102f859c0e9ff7e53d88080
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-04-01 12:22:16 +0200

    Modded typography and changed items in storage.

--- App.tsx
+++ App.tsx
@@ -1,30 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import warehouse from './assets/warehouse.jpg';
+import {StatusBar} from 'expo-status-bar';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import food from './assets/food.jpg';
 import Stock from './components/Stock.tsx';
+import {
+    useFonts,
+    // DarkerGrotesque_300Light,
+    DarkerGrotesque_400Regular,
+    // DarkerGrotesque_500Medium,
+    // DarkerGrotesque_600SemiBold,
+    // DarkerGrotesque_700Bold,
+    // DarkerGrotesque_800ExtraBold,
+    // DarkerGrotesque_900Black
+} from '@expo-google-fonts/darker-grotesque';
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.base}>
-                <Text style={{ color: '#33c', fontSize: 42 }}>Lager-Appen</Text>
-                <Image source={warehouse} style={{ width: 320, height: 240 }} />
-                <Stock />
-                <StatusBar style="auto" />
-            </View>
-        </SafeAreaView>
-    );
+    let [fontsLoaded] = useFonts({
+        DarkerGrotesque_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <Text>Not loaded yet</Text>;
+    } else {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.base}>
+                    <Text style={styles.title}>Lager-Appen</Text>
+                    <Image source={food} style={styles.mainImg}/>
+                    <Stock/>
+                    <StatusBar style="auto"/>
+                </View>
+            </SafeAreaView>
+        );
+    }
+
+
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fffdd0',
     },
     base: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'transparent',
         paddingLeft: 12,
         paddingRight: 12,
+    },
+    title: {
+        fontSize: 50.063,
+        textAlign: 'center',
+        fontFamily: 'DarkerGrotesque_400Regular'
+    },
+    mainImg: {
+        width: 320,
+        height: 240,
+        alignSelf: 'center',
     }
 });
--- components/Stock.tsx
+++ components/Stock.tsx
@@ -1,8 +1,22 @@
-import { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import {useState, useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import config from "../config/config.json";
+import {
+    useFonts,
+    // DarkerGrotesque_300Light,
+    DarkerGrotesque_400Regular,
+    // DarkerGrotesque_500Medium,
+    // DarkerGrotesque_600SemiBold,
+    // DarkerGrotesque_700Bold,
+    // DarkerGrotesque_800ExtraBold,
+    // DarkerGrotesque_900Black
+} from '@expo-google-fonts/darker-grotesque';
 
 function StockList() {
+    let [fontsLoaded] = useFonts({
+        DarkerGrotesque_400Regular,
+    });
+
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
@@ -11,21 +25,41 @@ function StockList() {
             .then(result => setProducts(result.data));
     }, []);
 
-    const list = products.map((product, index) => <Text key={index}>{ product.name } - { product.stock }</Text>);
+    const list = products.map((product, index) => <Text style={styles.stockList}
+                                                        key={index}>{product.name} - {product.stock}</Text>);
 
-    return (
-        <View>
-            {list}
-        </View>
-    );
+    if (!fontsLoaded) {
+        return <Text>Not loaded yet</Text>;
+    } else {
+        return (
+            <View>
+                {list}
+            </View>
+        );
+    }
+
 }
 
 
 export default function Stock() {
     return (
         <View>
-            <Text style={{color: '#333', fontSize: 24}}>Lagerförteckning</Text>
+            <Text style={styles.title}>Lagerförteckning</Text>
             <StockList/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 33.75,
+        alignSelf: 'center',
+        fontFamily: 'DarkerGrotesque_400Regular'
+    },
+    stockList: {
+        fontSize: 22.5,
+        lineHeight: 33.75,
+        alignSelf: 'center',
+        fontFamily: 'DarkerGrotesque_400Regular',
+    },
+});
