7ef0137cf70b7d9677ae1a2be1775e59a4e48a56
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-04-21 09:40:51 +0200

    Added avoid keyboard view for iOS, testing Android next

--- components/DeliveryForm.tsx
+++ components/DeliveryForm.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { ScrollView, Text, TextInput, Button } from "react-native";
-import { Base, Typography, Form } from '../styles';
+import {useState} from 'react';
+import {ScrollView, Text, TextInput, Button, KeyboardAvoidingView} from "react-native";
+import {Base, Typography, Form} from '../styles';
 
 import config from "../config/config.json";
 import deliveries from "../models/deliveries";
@@ -10,11 +10,11 @@ import {Product} from "../interfaces/product";
 import {ProductDropDown} from "./ProductDropDown";
 import {DateDropDown} from "./DateTimePicker";
 
-export default function DeliveryForm({ navigation, setProducts }) {
+export default function DeliveryForm({navigation, setProducts}) {
     const [currentProduct, setCurrentProduct] = useState<Partial<Product>>({});
     const [delivery, setDelivery] = useState<Partial<Delivery>>({});
 
-    async function addDelivery () {
+    async function addDelivery() {
         console.log(`CURRENT PRODUCT: ${JSON.stringify(currentProduct)}`);
         console.log(`DELIVERY: ${JSON.stringify(delivery)}`);
         try {
@@ -35,41 +35,47 @@ export default function DeliveryForm({ navigation, setProducts }) {
     }
 
     return (
-        <ScrollView style={{ ...Base.base }}>
-            <Text style={{ ...Typography.header2 }}>Ny inleverans</Text>
+        <ScrollView style={{...Base.base}}>
+            <KeyboardAvoidingView
+                style={{flex: 1}}
+                keyboardVerticalOffset={150}
+                behavior={"position"}
+            >
+                <Text style={{...Typography.header2}}>Ny inleverans</Text>
 
-            <Text style={{ ...Typography.label }}>Produkt</Text>
-            <ProductDropDown
-                delivery={delivery}
-                setDelivery={setDelivery}
-                setCurrentProduct={setCurrentProduct}
-            />
+                <Text style={{...Typography.label}}>Produkt</Text>
+                <ProductDropDown
+                    delivery={delivery}
+                    setDelivery={setDelivery}
+                    setCurrentProduct={setCurrentProduct}
+                />
 
-            <Text style={{ ...Typography.label }}>Antal</Text>
-            <TextInput
-                style={{ ...Form.input }}
-                onChangeText={(content: string) => {
-                    setDelivery({ ...delivery, amount: parseInt(content) })
-                }}
-                value={delivery?.amount?.toString()}
-                keyboardType="numeric"
-            />
+                <Text style={{...Typography.label}}>Antal</Text>
+                <TextInput
+                    style={{...Form.input}}
+                    onChangeText={(content: string) => {
+                        setDelivery({...delivery, amount: parseInt(content)})
+                    }}
+                    value={delivery?.amount?.toString()}
+                    keyboardType="numeric"
+                />
 
-            <Text style={{ ...Typography.label }}>Datum</Text>
-            <DateDropDown
-                style={{ ...Form.input }}
-                delivery={delivery}
-                setDelivery={setDelivery}
-            />
+                <Text style={{...Typography.label}}>Datum</Text>
+                <DateDropDown
+                    style={{...Form.input}}
+                    delivery={delivery}
+                    setDelivery={setDelivery}
+                />
 
-            <Text style={{ ...Typography.label }}>Kommentar</Text>
-            <TextInput
-                style={{ ...Form.input }}
-                onChangeText={(content: string) => {
-                    setDelivery({ ...delivery, comment: content})
-                }}
-                value={delivery?.comment}
-            />
+                <Text style={{...Typography.label}}>Kommentar</Text>
+                <TextInput
+                    style={{...Form.input}}
+                    onChangeText={(content: string) => {
+                        setDelivery({...delivery, comment: content})
+                    }}
+                    value={delivery?.comment}
+                />
+            </KeyboardAvoidingView>
 
             <Button
                 title="Gör inleverans"
--- styles/deliverieslist.js
+++ styles/deliverieslist.js
@@ -1,5 +1,4 @@
 export const deliveriesList = {
     padding: 11.25,
-    borderWidth: 1,
-    borderRadius: 22.5
+    borderWidth: 1
 };
